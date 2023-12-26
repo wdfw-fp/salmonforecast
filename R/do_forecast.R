df6cdd1e94c330b5e6fadc20ab8f0338b98ce71d
Mark Sorel <mark.sorel@dfw.wa.gov>
2023-12-26 10:13:02 -0800

    fill in outlines of plotiuing and wrapper function

--- R/do_forecast.R
+++ R/do_forecast.R
@@ -1,7 +1,7 @@
 #function to do forecast and return plots
 
 do_forecast<-function(
-    covariates<-c(#new
+    covariates=c(#new
       "lag1_log_JackOPI"
       ,"lag1_log_SmAdj"
       ,"lag1_NPGO"
@@ -16,7 +16,21 @@ do_forecast<-function(
       ,"SSH.AMJ"
       ,"UWI.SON"),
 
-    #add other inputs here (e.g., min_var, max_var, etc.)
+    leave_yrs=31,
+    TY_ensemble=16,
+    first_forecast_period = 1,
+    plot_results = FALSE,
+    write_model_summaries = TRUE,
+    forecast_period_start_m =  1, #inclusive
+    forecast_period_start_d =  1, #inclusive
+    stack_metric = "MAPE",
+    k=1,
+    min_vars=0,
+    max_vars=1,
+    forecast_type="preseason",
+    rolling_year_window=15,
+    num_models=10
+
 
   ){
 
@@ -24,7 +38,7 @@ do_forecast<-function(
   best_covariates<-all_subsets(series=dat,covariates=covariates,min=min_vars,max=max_vars,type=forecast_type,fit=FALSE)
 
   #generate one step ahead forecasts
-  results<-SalmonForecasting::one_step_ahead(series=dat,
+  results<-one_step_ahead(series=dat,
                                              leave_yrs=leave_yrs,
                                              TY_ensemble=TY_ensemble,
                                              covariates=
@@ -48,7 +62,7 @@ do_forecast<-function(
     dplyr::rename(model=rowname,model_name=value)
 
   # rolling performance / ensemble
-  rp<-SalmonForecasting::rolling_perf(one_aheads=results,
+  rp<-rolling_perf(one_aheads=results,
                                       series=dat,
                                       roll_years = rolling_year_window,
                                       mod_include = 10,
@@ -56,16 +70,17 @@ do_forecast<-function(
                                       model_list = model_list)
 
 
-  ens2<-SalmonForecasting::ensemble(forecasts=(rp$all_mods %<>% dplyr::group_by(year) %>% dplyr::mutate(rank=rank(MAPE)) %>% dplyr::ungroup()),
+  ens<-ensemble(forecasts=(rp$all_mods %<>% dplyr::group_by(year) %>% dplyr::mutate(rank=rank(MAPE)) %>% dplyr::ungroup()),
                                     series=dat,
                                     TY_ensemble=TY_ensemble,
                                     k=k,
                                     slide=15,
-                                    num_models=10,
+                                    num_models=num_models,
                                     stack_metric="MAPE")
   #plot and table
  plots_and_tables<- plot_table(
-    #add inputs from above
+   rp=rp,
+   ens=ens
   )
 
   return (best_covariates=best_covariates,
--- R/plot_table.R
+++ R/plot_table.R
@@ -1,11 +1,20 @@
 # function to make plots and tables
-
+library(knitr)
+library(kableExtra)
+library(ggplot2)
 plot_table<-function(
-    for_skill,
     rp,
-    ens,
+    ens
 
   ){
+
+  for_skill<-
+    rp$performance %>% mutate(model="Best_individual") %>% bind_rows(
+      ens$forecast_skill %>%filter(grepl("weight",model))
+    )  %>%
+    arrange(MAPE)
+
+
 
 
   Table2<-for_skill%>% mutate(model=sub("_"," ",model))%>% rename(Model=model)%>%
@@ -47,7 +56,7 @@ plot_table<-function(
   Table4<-results_best%>%
     rename(Year=year,Model=model_name,Abundance=abundance,`Predicted abundance`=predicted_abundance,`% error`=pct_errpr) %>%
     # dplyr::select(!c("train_test","Stacking_weight","model_name") & !c(covariates[covariates%in%names(results_best)]))%>%
-    kbl(caption = paste0("Table 4. One-year-ahead forecasts of OPI ", species," based on the ",best_model," model."),digits =2)%>%
+    kbl(caption = paste0("Table 4. One-year-ahead forecasts based on the ",best_model," model."),digits =2)%>%
     kable_classic(full_width = F, html_font = "Cambria")
 
 
@@ -61,17 +70,15 @@ plot_table<-function(
 
 
 
-  Figure2<-ggplot(rp$top_mods %>% filter(rank==1) %>% mutate(model="Best_individual") %>% bind_rows(
-    ens$ensembles %>% left_join(dat %>% dplyr::select(year,abundance)) %>% mutate(model_name=model)),aes(x=year,y=predicted_abundance,col=model))+
-    geom_line()+
-    geom_line(data=results_best,mapping=aes(x=year,y=predicted_abundance),size=1.1,col="black")+
-    geom_line(data=results_old$forecasts,mapping=aes(x=year,y=predicted_abundance),size=1.25,col="grey")+
-    geom_point(aes(x=year,y=abundance),color="black")+
-    ylim(0,NA)+
-    scale_x_continuous(breaks=unique(results_best$year))+
-    theme(legend.position = "none")+
-    ylab("Ocean abundance")+xlab("")
-  #theme(legend.key.size=unit(.15,'cm'),legend.position = "top")
+  Figure2<-ggplot(rp$top_mods %>% filter(rank==1) %>% mutate(model="Best_individual")%>% bind_rows(
+    ens$ensembles%>% left_join(dat %>% dplyr::select(year,abundance)) %>% mutate(model_name=model)),aes(x=year,y=predicted_abundance,col=model))+
+  geom_line()+
+  geom_point(aes(x=year,y=abundance),color="black")+
+  ylim(0,NA)+
+  scale_x_continuous(breaks=unique(results_best$year))+
+  theme(legend.position = "none")+
+  ylab("Ocean abundance")+xlab("")+
+  theme(legend.key.size=unit(.15,'cm'),legend.position = "top")
 
 
   Figure3<-ggplot((results_best ) %>% mutate(Model=sub("_"," ",model_name)) %>%  mutate(pct_error=log10(((predicted_abundance-abundance)/abundance)+1)) %>% filter(year<2023),
@@ -84,7 +91,7 @@ return(
     Table3=Table3,
     Table4=Table4,
     Figure1=Figure1,
-    Figure2-Figure2
+    Figure2=Figure2
   )
 )
 
