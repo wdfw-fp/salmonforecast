fd75cd40a41b5809ecfc61be5b08c12e7fbd53a9
Xiaotian Yuan <tjyuanxiaotian@gmail.com>
2024-01-08 16:07:05 -0800

    Extract the plots_and_tables element from the results

--- tests/testthat/test_do_forecast.R
+++ tests/testthat/test_do_forecast.R
@@ -17,23 +17,23 @@ test_that("Test do_forecast function", {
     ,"SSH.AMJ"
     ,"UWI.SON"),
 
-  leave_yrs=31,
-  TY_ensemble=16,
-  first_forecast_period = 1,
-  plot_results = FALSE,
-  write_model_summaries = TRUE,
-  forecast_period_start_m =  1, #inclusive
-  forecast_period_start_d =  1, #inclusive
-  stack_metric = "MAPE",
-  k=1,
-  min_vars=0,
-  max_vars=1,
-  forecast_type="preseason",
-  rolling_year_window=15,
-  num_models=10
+    leave_yrs=31,
+    TY_ensemble=16,
+    first_forecast_period = 1,
+    plot_results = FALSE,
+    write_model_summaries = TRUE,
+    forecast_period_start_m =  1, #inclusive
+    forecast_period_start_d =  1, #inclusive
+    stack_metric = "MAPE",
+    k=1,
+    min_vars=0,
+    max_vars=1,
+    forecast_type="preseason",
+    rolling_year_window=15,
+    num_models=10
 
 
-)
+  )
 
   # Extract the plots_and_tables element
   plots_and_tables_output <- output$plots_and_tables
