a5fa84c7c1f0291daeb911fca04a1ab46cb72871
Xiaotian Yuan <tjyuanxiaotian@gmail.com>
2024-01-22 22:43:39 -0800

    add MASE

--- R/calculate_performance_metrics.R
+++ R/calculate_performance_metrics.R
@@ -1,12 +1,11 @@
 #' @name calculate_performance_metrics
 #' @title Calculate performance metrics
-#' @description This function calculates performance metrics (MAPE, RMSE, MSA) for a given set of predictions and observations.
+#' @description This function calculates performance metrics (MAPE, RMSE, MSA, MASE) for a given set of predictions and observations.
 #'
 #' @param predicted Predicted values.
 #' @param observed Observed values.
 #' @param weighted Logical, indicating whether to calculate weighted metrics.
 #' @return A data frame with calculated performance metrics.
-#' @export
 calculate_performance_metrics <- function(predicted, observed, weighted = FALSE) {
   # Combine predicted and observed into a data frame
   data <- data.frame(predicted, observed)
@@ -20,9 +19,11 @@ calculate_performance_metrics <- function(predicted, observed, weighted = FALSE)
       MAPE = NA,
       RMSE = NA,
       MSA = NA,
+      MASE = NA,
       RMSE_weighted = NA,
       MSA_weighted = NA,
-      MAPE_weighted = NA
+      MAPE_weighted = NA,
+      MASE_weighted = NA
     ))
   }
 
@@ -33,10 +34,15 @@ calculate_performance_metrics <- function(predicted, observed, weighted = FALSE)
   error <- observed - predicted
   APE <- abs(error / observed)
 
+  # Calculate MASE
+  naive_forecast <- c(predicted[1], predicted[1:length(predicted)-1])
+  MASE <- mean(abs(observed - naive_forecast))
+
   performance_metrics <- data.frame(
     MAPE = mean(APE, na.rm = TRUE) * 100,
     RMSE = sqrt(mean(error^2, na.rm = TRUE)),
-    MSA = 100 * (exp(mean(abs(log(observed / predicted)), na.rm = TRUE)) - 1)
+    MSA = 100 * (exp(mean(abs(log(observed / predicted)), na.rm = TRUE)) - 1),
+    MASE = MASE
   )
 
   if (weighted) {
@@ -44,15 +50,16 @@ calculate_performance_metrics <- function(predicted, observed, weighted = FALSE)
     weighted_RMSE <- weighted_metric(error, observed, "RMSE")
     weighted_MSA <- weighted_metric(error, observed, "MSA")
     weighted_MAPE <- weighted_metric(APE, observed, "MAPE")
+    weighted_MASE <- weighted_metric(abs(error - mean(error)), observed, "MASE")
 
     performance_metrics$RMSE_weighted <- weighted_RMSE
     performance_metrics$MSA_weighted <- weighted_MSA
     performance_metrics$MAPE_weighted <- weighted_MAPE
+    performance_metrics$MASE_weighted <- weighted_MASE
   }
 
   return(performance_metrics)
 }
-
 weighted_metric <- function(error, observed, metric) {
   # Adjusted weighted metric calculation to handle NAs
   weights <- !is.na(error) & !is.na(observed)
